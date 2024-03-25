002ef62a0dfb198e33e8d75b48de2d8ec9811735
Shahzod <yuldashev.1996@gmail.com>
2024-03-25 12:36:18 -0400

    Added pop up Car Details modal

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -1,21 +1,37 @@
-'use client'
+'use client';
 
-import { CustomButtonProps } from '@/types'
-import Image from 'next/image'
+import { CustomButtonProps } from '@/types';
+import Image from 'next/image';
 
-const CustomButton = ({ title, containerStyles, handleClick, btnType }: CustomButtonProps) => { // passing type props from CustomButtonProps
-  return (
-    <button 
-    disabled={false}
-    type={btnType || 'button'}
-    className={`custom-btn ${containerStyles}`}
-    onClick={(handleClick) => {}}
-    >
-        <span className={`flex-1`}>
-            {title}
-        </span>
-    </button>
-  )
-}
+const CustomButton = ({     // passing type props from CustomButtonProps
+    title,
+    containerStyles,
+    handleClick,
+    btnType,
+    textStyles,
+    rightIcon
+}: CustomButtonProps) => { 
 
-export default CustomButton
+    return (
+        <button
+            disabled={false}
+            type={btnType || 'button'}
+            className={`custom-btn ${containerStyles}`}
+            onClick={handleClick}
+        >
+            <span className={`flex-1 ${textStyles}`}>{title}</span>
+            {rightIcon && (
+                <div className='relative w-6 h-6'>
+                    <Image
+                        src={rightIcon}
+                        alt='right icon'
+                        fill
+                        className='object-contain'
+                    />
+                </div>
+            )}
+        </button>
+    );
+};
+
+export default CustomButton;
